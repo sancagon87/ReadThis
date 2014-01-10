5b16a2a4ade7a9603e45e6da21563e934374df56
sancagon87 <sancagon87@gmail.com>
2014-01-10 02:54:32 -0200

    v0.2 - Cambio de paginas a izq y der. implementado (doble pagina)

--- ReadThis.js
+++ ReadThis.js
@@ -15,12 +15,36 @@
 		{
 			this.css("background", opciones.background_color);
 		}
-		setUp(this);
+		this.css("overflow", "hidden");
+		this.css("height",opciones.height);
+		this.css("width",opciones.width);
+		if(opciones.pages == "single")
+		{	
+			alert("single");
+			setUpSingle(this);
+		} else {
+			alert("doble");
+			setUpDoble(this);
+		}
 	};
-	
-	function setUp(OBJ)
+	function setUpSingle(OBJ)
+	{	
+		var imagenes = new Array();
+		
+		//GUARDAR IMAGENES COMO OBJETOS Image
+		//Y CREAR DIVS CORRESPONDIENTES
+		for(var i = 0; i < largo; i++)
+		{
+			imagenes[i] = new Image();
+			imagenes[i].src = OBJ.data("opciones").array[i];
+			OBJ.append("<div id='" + i + "'><img src=''></img></div>");
+		}
+		OBJ.data("imagenes", imagenes);
+		//CREAR LISTENERS
+		crearListeners(OBJ);
+	}
+	function setUpDoble(OBJ)
 	{
-
 		var largo = OBJ.data("opciones").array.length;
 		var imagenes = new Array();
 		var primeraDoble = false, listo = false;
@@ -28,14 +52,17 @@
 		var largoB = new Array();
 		var j = 0, k = 0;
 		
+		//GUARDAR IMAGENES COMO OBJETOS Image
 		for(var i = 0; i < largo; i++)
 		{
 			imagenes[i] = new Image();
 			imagenes[i].src = OBJ.data("opciones").array[i];
 		}
+		OBJ.data("imagenes", imagenes);
 		
-		var doblePar = false;
+		var doblePar = false, derecha = true, doble = false;
 		
+		//DETERMINAR CUALES SON PAGINAS DOBLES
 		for(var i = 0; i < largo; i++)
 		{
 			if(i == 0)
@@ -63,6 +90,7 @@
 			}	
 			if(i == largo-1)
 			{
+				//CREAR DIVS CORRESPONDIENTES
 				alert("LARGOS: " + largos);
 				for(var h = 0; h < largo; h++)
 				{	
@@ -71,35 +99,94 @@
 						if(primeraDoble)
 						{
 							meterRelleno(OBJ);
+							doble = true;
 						}
 					} else {
 						if(largos[h] == "doble")
 						{
-							if(h%2 == 0)
+							doble = true;
+							if((h+1)%2 == 0)
 							{
-								if(!doblePar)
-								{
-									meterRelleno(OBJ);
-									doblePar = true;
-								}
-							} else {
 								if(doblePar)
 								{
 									meterRelleno(OBJ);
-									doblePar = false;
 								}
+								doblePar = true;
+							} else {
+								if(!doblePar)
+								{
+									meterRelleno(OBJ);
+								}
+								doblePar = false;
 							}
+						} else {
+							doble = false;
 						}
 					}
-					OBJ.append("<div id='" + h + "'><img src=''></img></div>");
+					if(doble)
+					{
+						derecha = false;
+						OBJ.append("<div id='" + h + "' class='doble hidden'><img src=''></img></div>");
+					} else if(derecha) {
+						derecha = false;
+						OBJ.append("<div id='" + h + "' class='derecha hidden'><img src=''></img></div>");
+					} else {
+						derecha = true;
+						OBJ.append("<div id='" + h + "' class='izquierda hidden'><img src=''></img></div>");
+					}
 				}
 			}
 		}
+		$("#ReadThis").contents().first().removeClass("hidden");
+		$("#ReadThis").contents().first().addClass("mostrar");
+		//CREAR LISTENERS
+		crearListeners(OBJ);
 	}
 	function meterRelleno(OBJ)
 	{
-		OBJ.append("<div class='relleno'></div>");
+		OBJ.append("<div class='relleno hidden'></div>");
 	}
+	function crearListeners(OBJ)
+	{
+		OBJ.click(function(e) {
+			if(e.pageX >= ($(document).width()/2))
+			{
+				OBJ.flipPage("right");
+			} else {
+				OBJ.flipPage("left");
+			}
+		});
+	}
+	$.fn.flipPage = function(dir)
+	{
+		var ultimo = this.data("opciones").array.length - 1;
+		
+		if(this.data("opciones").pages == "single")
+		{
+			
+		} else {
+			if( dir == "right" && (this.contents(".mostrar").last().attr("id") != ultimo) )
+			{
+				this.contents(".mostrar").last().next().addClass("proximo");
+				if( !this.contents(".mostrar").last().next().hasClass("doble") )
+				{
+					this.contents(".proximo").next().addClass("proximo");
+				}
+				this.contents(".mostrar").each(function(){$(this).addClass("hidden"); $(this).removeClass("mostrar");});
+				this.contents(".proximo").each(function(){$(this).addClass("mostrar");});
+				this.contents(".proximo").each(function(){$(this).removeClass("hidden"); $(this).removeClass("proximo");});
+			} else if( dir == "left" && (this.contents(".mostrar").first().attr("id") != 0) ) {
+				this.contents(".mostrar").first().prev().addClass("proximo");
+				if( !this.contents(".mostrar").first().prev().hasClass("doble") )
+				{
+					this.contents(".proximo").prev().addClass("proximo");
+				}
+				this.contents(".mostrar").each(function(){$(this).addClass("hidden"); $(this).removeClass("mostrar");});
+				this.contents(".proximo").each(function(){$(this).addClass("mostrar");});
+				this.contents(".proximo").each(function(){$(this).removeClass("hidden"); $(this).removeClass("proximo");});
+			}
+		}
+	};
 }( jQuery ));
 
 
